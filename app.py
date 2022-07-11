56feacde1d7ca0346e3312ca4259010db9bb0ca3
ckstn0225 <ckstn0225@gmail.com>
2022-07-11 15:29:50 +0900

    app.py 양식 추가

--- app.py
+++ app.py
@@ -9,29 +9,20 @@ client = MongoClient('mongodb+srv://Test:sparta@cluster0.iwfou.mongodb.net/?retr
 db = client.dbsparta
 
 
+## HTML을 주는 부분
+# 들어오면 로그인 화면으로
 @app.route('/')
 def home():
+   return render_template('login.html')
 
+@app.route('/memo', methods=['GET'])
+def listing():
+    return jsonify({'all_articles':articles})
 
-    return render_template('index.html')
-
-
-@app.route("/home", methods=["POST"])
-def homework_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {
-        'name':name_receive,
-        'comment':comment_receive
-    }
-    db.comments.insert_one(doc)
-    return jsonify({'msg': '응원 완료!'})
-
-
-@app.route("/home", methods=["GET"])
-def homework_get():
-    comment_list = list(db.comments.find({}, {'_id': False}))
-    return jsonify({'comments': comment_list})
+## API 역할을 하는 부분
+@app.route('/memo', methods=['POST'])
+def saving():
+    return jsonify({'msg':'저장이 완료되었습니다!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
