57f45b38c2d5b212c3765481fe6ca29c1dff0aa3
Rzreo <zuzu5313@naver.com>
2022-07-11 22:02:36 +0900

    방 추가하기 기능과 전체 방 출력 구현

--- app.py
+++ app.py
@@ -40,5 +40,42 @@ def listing():
 def saving():
     return jsonify({'msg':'저장이 완료되었습니다!'})
 
+##room post와 get
+@app.route("/room", methods=["POST"])
+def room_post():
+    room_list = list(db.room.find({}, {'_id': False}))
+    count = len(room_list) + 1
+    gname_receive = request.form['gname_give']
+    title_receive = request.form['title_give']
+    desc_receive = request.form['desc_give']
+    totalnum_receive = request.form['totalnum_give']
+    date_receive = request.form['date_give']
+    time_receive = request.form['time_give']
+    nick_receive = request.form['nick_give']
+    plist = []
+    plist.append(nick_receive)
+
+    doc = {
+        'rid':count,
+        'gname':gname_receive,
+        'title':title_receive,
+        'desc':desc_receive,
+        'totalnum':totalnum_receive,
+        'num':1,
+        'date':date_receive,
+        'time':time_receive,
+        'nick':nick_receive,
+        'participate':plist
+    }
+    db.room.insert_one(doc)
+
+    return jsonify({'msg':'모집 게시글 작성 완료!'})
+
+@app.route("/room", methods=["GET"])
+def room_get():
+    room_list = list(db.room.find({},{'_id':False}))
+    return jsonify({'room':room_list})
+
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
